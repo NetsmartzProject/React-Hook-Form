21c9befa1f6f5496e5fd0af8f3c61c96bba85ec1
Abhinandan <royabhinandan.002@gmail.com>
2024-01-22 14:31:47 +0530

    react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import { Route, Router, Routes } from 'react-router-dom';
+// import { Form, Form1 } from './form/Form1';
+
+// function App() {
+//   return (
+//     <div className="App">
+//    <Router>
+//     <Routes>
+//       <Route path='/' element={<Form1/>}></Route>
+//     </Routes>
+//    </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter as Router, Routes,  } from 'react-router-dom';
+import { Form, Form1 } from './form/Form1';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Form1 />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
